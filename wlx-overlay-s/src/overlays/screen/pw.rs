d2e7321f977c305f3c1b89c0a474d7a4c0455d88
galister <22305755+galister@users.noreply.github.com>
2025-12-28 14:19:16 +0900

    make screen select toast not urgent due to kde

--- wlx-overlay-s/src/overlays/screen/pw.rs
+++ wlx-overlay-s/src/overlays/screen/pw.rs
@@ -87,7 +87,7 @@ pub(super) fn select_pw_screen(
                 task::Poll::Pending => {
                     if Instant::now() >= print_at {
                         log::info!("{instructions}");
-                        if let Ok(id) = app.dbus.notify_send(instructions, "", 2, 30, 0, true) {
+                        if let Ok(id) = app.dbus.notify_send(instructions, "", 1, 60, 0, true) {
                             notify = Some(id);
                         }
                         break;
