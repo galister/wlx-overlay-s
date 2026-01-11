b48320c7bc0b4e225021a846dab5b699509123c0
amoshydra <8733840+amoshydra@users.noreply.github.com>
2026-01-11 19:12:44 +0800

    feat: use contextual label for full frame 3d base on stereo mode
    show Full-SBS when stereo is LeftRight or RightLeft
    show Full-TAB when stereo is TopBottom
    show Full-BAT when stereo is BottomTop

--- wayvr/src/overlays/edit/stereo.rs
+++ wayvr/src/overlays/edit/stereo.rs
@@ -2,6 +2,7 @@ use crate::overlays::edit::{
     EditModeWrapPanel,
     sprite_tab::{SpriteTabHandler, SpriteTabKey},
 };
+use wgui::{components::checkbox::ComponentCheckbox, i18n::Translation, parser::Fetchable};
 use wlx_common::overlays::{BackendAttribValue, StereoMode};
 
 static STEREO_NAMES: [&str; 5] = ["none", "leftright", "rightleft", "topbottom", "bottomtop"];
@@ -9,12 +10,21 @@ static STEREO_NAMES: [&str; 5] = ["none", "leftright", "rightleft", "topbottom",
 pub fn new_stereo_tab_handler(
     panel: &mut EditModeWrapPanel,
 ) -> anyhow::Result<SpriteTabHandler<StereoMode>> {
+    // Fetch the checkbox component first, before creating the closure
+    let checkbox = panel
+        .parser_state
+        .fetch_component_as::<ComponentCheckbox>("stereo_full_frame_box")?;
+
     SpriteTabHandler::new(
         panel,
         "stereo",
         &STEREO_NAMES,
-        Box::new(|_common, state| {
+        Box::new(move |common, state| {
             let stereo = *state;
+
+            let translation = get_stereo_full_frame_translation(&stereo);
+            checkbox.set_text(common, Translation::from_translation_key(translation));
+
             Box::new(move |app, owc| {
                 owc.backend
                     .set_attrib(app, BackendAttribValue::Stereo(stereo));
@@ -46,5 +56,14 @@ impl SpriteTabKey for StereoMode {
                 panic!("cannot translate to stereo mode: {key}")
             }
         }
+    }
+}
+
+pub fn get_stereo_full_frame_translation(stereo: &StereoMode) -> &'static str {
+    match stereo {
+        StereoMode::LeftRight | StereoMode::RightLeft => "EDIT_MODE.STEREO_3D_MODE.FULL_FRAME_SBS",
+        StereoMode::TopBottom => "EDIT_MODE.STEREO_3D_MODE.FULL_FRAME_TAB",
+        StereoMode::BottomTop => "EDIT_MODE.STEREO_3D_MODE.FULL_FRAME_BAT",
+        StereoMode::None => "EDIT_MODE.STEREO_3D_MODE.FULL_FRAME",
     }
 }
